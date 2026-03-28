6eea780ea8411370a6d9ab1858d2993fbabcdc7d
eellama <elena0411yu@gmail.com>
2026-03-28 00:26:59 -0700

    updated deseq files 03/28
    still need to update labels, tell Bessie, add labels for specific genus to match core, maybe also make plots not glommed to genus but ASVs

--- DESeq/deseq_2.R
+++ DESeq/deseq_2.R
@@ -1,4 +1,6 @@
 #### Load packages ####
+
+library(patchwork)
 library(tidyverse)
 library(phyloseq)
 library(DESeq2)
@@ -16,45 +18,28 @@ sample_data(hiv_final) <- sample_data(IL6_binning_update)
 # IL6 Hi vs Lo on HIV+ patients
 hiv_plwh <- subset_samples(hiv_final, HIV_Status == "Positive")
 hiv_plwh_plus1 <- transform_sample_counts(hiv_plwh, function(x) x+1)
-hiv_plwh_deseq <- phyloseq_to_deseq2(hiv_plwh_plus1, ~`IL6_bin`)
+hiv_plwh_genus <- tax_glom(hiv_plwh_plus1, taxrank = "Genus")
+hiv_plwh_deseq <- phyloseq_to_deseq2(hiv_plwh_genus, ~`IL6_bin`)
 DESEQ_hiv_plwh <- DESeq(hiv_plwh_deseq)
 HIV_plwh_res <- results(DESEQ_hiv_plwh, tidy=TRUE,
                         contrast = c("IL6_bin","high","low"))
 
-# glom to Genus
-HIV_plwh_ASVs <- HIV_plwh_res %>%
-  dplyr::rename(ASV=row)
-HIV_plwh_res_vec <- HIV_plwh_ASVs %>%
-  pull(ASV)
-HIV_plwh_genus_deseq <- prune_taxa(HIV_plwh_res_vec,hiv_final)
-HIV_plwh_ASVs <- tax_table(HIV_plwh_genus_deseq) %>% as.data.frame() %>%
-  rownames_to_column(var="ASV") %>%
-  right_join(HIV_plwh_ASVs) %>%
-  arrange(log2FoldChange) %>%
-  mutate(Genus = make.unique(Genus)) %>%
-  mutate(Genus = factor(Genus, levels=unique(Genus)))
+# Glom to Genus
+HIV_plwh_res <- HIV_plwh_res %>%
+  dplyr::rename(Genus_ID=row)
+
+tax_df <- tax_table(hiv_plwh_genus) %>%
+  as.data.frame() %>%
+  rownames_to_column("Genus_ID")
+
+HIV_plwh_res <- right_join(HIV_plwh_res, tax_df, by = "Genus_ID") %>%
+  mutate(Genus = make.unique(as.character(Genus)))
 
 #### Visualizing Data ####
-# Prune phyloseq file
-HIV_plwh_sigASVs <- HIV_plwh_res %>% 
-  filter(padj<0.01 & abs(log2FoldChange)>2) %>%
-  dplyr::rename(ASV=row)
-View(HIV_plwh_sigASVs)
-# Get only significant asv names
-HIV_plwh_sigASVs_vec <- HIV_plwh_sigASVs %>%
-  pull(ASV)
-
-HIV_plwh_deseq <- prune_taxa(HIV_plwh_sigASVs_vec,hiv_final)
-HIV_plwh_sigASVs <- tax_table(HIV_plwh_deseq) %>% as.data.frame() %>%
-  rownames_to_column(var="ASV") %>%
-  right_join(HIV_plwh_sigASVs) %>%
-  arrange(log2FoldChange) %>%
-  mutate(Genus = make.unique(Genus)) %>%
-  mutate(Genus = factor(Genus, levels=unique(Genus)))
 
 ## Volcano plot: effect size VS significance
 # HIV+ IL6 Binned
-HIV_plwh_genus_vol_plot <- HIV_plwh_ASVs %>%
+HIV_plwh_genus_vol_plot <- HIV_plwh_res %>%
   mutate(category = case_when(log2FoldChange > 2  ~ "Upregulated",
                               log2FoldChange < -2 ~ "Downregulated",
                               padj < 0.05 ~ "Significant",
@@ -65,14 +50,82 @@ HIV_plwh_genus_vol_plot <- HIV_plwh_ASVs %>%
   geom_hline(yintercept = -log10(0.05), linetype = "dashed", colour = "grey") +
   labs(x = "Fold Change (log[2])", y = "-log[10] (Padj)") +
   geom_point(aes(x=log2FoldChange, y= -log10(padj), col=category)) +
-  scale_x_continuous(limits = c(-8,5), breaks = seq(-8,4,2)) + 
+  scale_x_continuous(limits = c(-4,4), breaks = seq(-4,4,2)) + 
   theme_bw(9) # remove background gray grid
 ggsave(filename="HIV_plwh_genus_vol_plot.png",HIV_plwh_genus_vol_plot)
 
 ## Bar graph
+# Filter for significant genera
+HIV_plwh_sig_genus <- HIV_plwh_res %>%
+  filter(padj < 0.01 & abs(log2FoldChange) > 2)
+
 # PLWH IL-6
-PLWH_bar <- ggplot(HIV_plwh_sigASVs) +
-  geom_bar(aes(x=Genus, y=log2FoldChange, fill = log2FoldChange), stat="identity") +
+PLWH_bar <- ggplot(HIV_plwh_sig_genus) +
+  geom_bar(aes(x=reorder(Genus, log2FoldChange), y=log2FoldChange, fill = log2FoldChange), stat="identity") +
   geom_errorbar(aes(x=Genus, ymin=log2FoldChange-lfcSE, ymax=log2FoldChange+lfcSE)) +
-  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
-ggsave(filename="PLWH_bar.png", PLWH_bar, width = 30, height = 15, units = "cm")
+  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5)) +
+  theme_bw() +
+  labs(x = "Genus", y = "log[2] Fold Change")
+ggsave(filename="PLWH_bar.png", PLWH_bar)
+
+#### ALL STATUS ####
+#### DESeq ####
+
+# IL6 Hi vs Lo on all patients
+hiv_plus1 <- transform_sample_counts(hiv_final, function(x) x+1)
+hiv_genus <- tax_glom(hiv_plus1, taxrank = "Genus")
+hiv_deseq <- phyloseq_to_deseq2(hiv_genus, ~`IL6_bin`)
+DESEQ_hiv <- DESeq(hiv_deseq)
+HIV_res <- results(DESEQ_hiv, tidy=TRUE,
+                        contrast = c("IL6_bin","high","low"))
+
+# Glom to Genus
+HIV_res <- HIV_res %>%
+  dplyr::rename(Genus_ID=row)
+
+tax_all_df <- tax_table(hiv_genus) %>%
+  as.data.frame() %>%
+  rownames_to_column("Genus_ID")
+
+HIV_res <- right_join(HIV_res, tax_all_df, by = "Genus_ID") %>%
+  mutate(Genus = make.unique(as.character(Genus)))
+
+#### Visualizing Data ####
+
+## Volcano plot: effect size VS significance
+# HIV+ IL6 Binned
+HIV_vol_plot <- HIV_res %>%
+  mutate(category = case_when(log2FoldChange > 2  ~ "Upregulated",
+                              log2FoldChange < -2 ~ "Downregulated",
+                              padj < 0.05 ~ "Significant",
+                              TRUE ~ "Not Significant")) %>%
+  ggplot() +
+  geom_vline(xintercept = -2, linetype = "dashed", colour = "grey") +
+  geom_vline(xintercept = 2, linetype = "dashed", colour = "grey") +
+  geom_hline(yintercept = -log10(0.05), linetype = "dashed", colour = "grey") +
+  labs(x = "Fold Change (log[2])", y = "-log[10] (Padj)") +
+  geom_point(aes(x=log2FoldChange, y= -log10(padj), col=category)) +
+  scale_x_continuous(limits = c(-4,4), breaks = seq(-4,4,2)) + 
+  theme_bw(9) # remove background gray grid
+ggsave(filename="all_status_vol_plot.png",HIV_vol_plot)
+
+## Bar graph
+# Filter for significant genera
+HIV_sig_genus <- HIV_res %>%
+  filter(padj < 0.01 & abs(log2FoldChange) > 2)
+
+# PLWH IL-6
+all_bar <- ggplot(HIV_sig_genus) +
+  geom_bar(aes(x=reorder(Genus, log2FoldChange), y=log2FoldChange, fill = log2FoldChange), stat="identity") +
+  geom_errorbar(aes(x=Genus, ymin=log2FoldChange-lfcSE, ymax=log2FoldChange+lfcSE)) +
+  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5)) +
+  theme_bw() +
+  labs(x = "Genus", y = "log[2] Fold Change")
+ggsave(filename="all_bar.png", all_bar)
+
+# Put together
+bars <- all_bar + PLWH_bar
+volcanos <- HIV_vol_plot + HIV_plwh_genus_vol_plot
+
+ggsave(filename="bars.png", bars)
+ggsave(filename="volcanos.png", volcanos)
