6a899750885152dd3e66ece104f7938f1cd4838e
eellama <elena0411yu@gmail.com>
2026-03-31 00:45:58 -0700

    better DESeq plots hopefully

--- DESeq/deseq labelling volcano plot (fail because nothing is significant and core apparently).R
+++ DESeq/deseq labelling volcano plot (fail because nothing is significant and core apparently).R
@@ -1,10 +1,10 @@
 #### Load packages ####
-
 library(patchwork)
 library(tidyverse)
 library(phyloseq)
 library(DESeq2)
-library(ggplot2)
+library(microbiome)
+library(ggrepel)
 
 #### Load data ####
 load("hiv_final.RData")
@@ -12,6 +12,33 @@ load("hiv_final.RData")
 IL6_binning_update <- data.frame(sample_data(hiv_final)) %>%
   mutate(IL6_bin = ifelse(IL.6_pg_mL > 5, "high", "low"))
 sample_data(hiv_final) <- sample_data(IL6_binning_update)
+
+#### Core ####
+# subset IL-6 bins for core microbiome
+il6_low <- subset_samples(hiv_final, `IL6_bin`=="low")
+il6_high <- subset_samples(hiv_final, `IL6_bin`=="high")
+
+# identify members in each group that meet thresholds
+low_ASVs <- core_members(il6_low, detection=0, prevalence = 0.7)
+high_ASVs <- core_members(il6_high, detection=0, prevalence = 0.7)
+
+ggVennDiagram(x=list(high_ASVs, low_ASVs))
+
+# Get taxonomy from full dataset
+tax_df_full <- tax_table(hiv_final) %>%
+  as.data.frame() %>%
+  rownames_to_column("ASV")
+
+# Map ASVs to Genus
+low_core_genus <- tax_df_full %>%
+  filter(ASV %in% low_ASVs) %>%
+  pull(Genus) %>%
+  unique()
+
+high_core_genus <- tax_df_full %>%
+  filter(ASV %in% high_ASVs) %>%
+  pull(Genus) %>%
+  unique()
 
 #### DESeq ####
 
@@ -35,24 +62,36 @@ tax_df <- tax_table(hiv_plwh_genus) %>%
 HIV_plwh_res <- right_join(HIV_plwh_res, tax_df, by = "Genus_ID") %>%
   mutate(Genus = make.unique(as.character(Genus)))
 
+HIV_plwh_res <- HIV_plwh_res %>%
+  mutate(core_status = case_when(
+    Genus %in% high_core_genus & Genus %in% low_core_genus ~ "Core Both",
+    Genus %in% high_core_genus ~ "Core High",
+    Genus %in% low_core_genus ~ "Core Low",
+    TRUE ~ "Non-core"
+  ))
+
 #### Visualizing Data ####
 
 ## Volcano plot: effect size VS significance
 # HIV+ IL6 Binned
-HIV_plwh_genus_vol_plot <- HIV_plwh_res %>%
+HIV_plwh_genus_vol <- HIV_plwh_res %>%
   mutate(category = case_when(log2FoldChange > 2  ~ "Upregulated",
                               log2FoldChange < -2 ~ "Downregulated",
                               padj < 0.05 ~ "Significant",
-                              TRUE ~ "Not Significant")) %>%
-  ggplot() +
-  geom_vline(xintercept = -2, linetype = "dashed", colour = "grey") +
-  geom_vline(xintercept = 2, linetype = "dashed", colour = "grey") +
-  geom_hline(yintercept = -log10(0.05), linetype = "dashed", colour = "grey") +
-  labs(x = "Fold Change (log[2])", y = "-log[10] (Padj)") +
-  scale_color_discrete(name="") +
-  geom_point(aes(x=log2FoldChange, y= -log10(padj), col=category)) +
-  scale_x_continuous(limits = c(-4,4), breaks = seq(-4,4,2)) + 
-  theme_bw(9) # remove background gray grid
+                              TRUE ~ "Not Significant"))
+
+genes_to_label <- HIV_plwh_genus_vol[HIV_plwh_genus_vol$core_status != "Non-core", "Genus"]
+
+HIV_plwh_genus_vol_plot <- ggplot(HIV_plwh_genus_vol,
+                                  aes(x = log2FoldChange, y = -log10(padj))) +
+  geom_point(aes(color = core_status)) +
+  ggrepel::geom_label_repel(data = subset(HIV_plwh_genus_vol, padj < 0.05 & abs(log2FoldChange) > 2 & core_status != "Non-core"), 
+                            aes(label = Genus), 
+                            color = "black", 
+                            show.legend = FALSE, 
+                            max.overlaps = Inf) +
+  theme_bw()
+
 ggsave(filename="HIV_plwh_genus_vol_plot.png",HIV_plwh_genus_vol_plot)
 
 ## Bar graph
@@ -91,24 +130,40 @@ tax_all_df <- tax_table(hiv_genus) %>%
 HIV_res <- right_join(HIV_res, tax_all_df, by = "Genus_ID") %>%
   mutate(Genus = make.unique(as.character(Genus)))
 
+HIV_res <- HIV_res %>%
+  mutate(core_status = case_when(
+    Genus %in% high_core_genus & Genus %in% low_core_genus ~ "Core Both",
+    Genus %in% high_core_genus ~ "Core High",
+    Genus %in% low_core_genus ~ "Core Low",
+    TRUE ~ "Non-core"
+  ))
+
 #### Visualizing Data ####
 
 ## Volcano plot: effect size VS significance
 # HIV+ IL6 Binned
-HIV_vol_plot <- HIV_res %>%
+
+HIV_vol <- HIV_res %>%
   mutate(category = case_when(log2FoldChange > 2  ~ "Upregulated",
                               log2FoldChange < -2 ~ "Downregulated",
                               padj < 0.05 ~ "Significant",
-                              TRUE ~ "Not Significant")) %>%
-  ggplot() +
-  geom_vline(xintercept = -2, linetype = "dashed", colour = "grey") +
-  geom_vline(xintercept = 2, linetype = "dashed", colour = "grey") +
+                              TRUE ~ "Not Significant"))
+
+genes_label <- HIV_vol[HIV_vol$core_status != "Non-core", "Genus"]
+
+HIV_vol_plot <- ggplot(HIV_vol,
+                       aes(x = log2FoldChange, y = -log10(padj))) +
+  geom_vline(xintercept = c(-2, 2), linetype = "dashed", colour = "grey") +
   geom_hline(yintercept = -log10(0.05), linetype = "dashed", colour = "grey") +
-  labs(x = "Fold Change (log[2])", y = "-log[10] (Padj)") +
-  geom_point(aes(x=log2FoldChange, y= -log10(padj), col=category)) +
-  scale_x_continuous(limits = c(-4,4), breaks = seq(-4,4,2)) + 
-  scale_color_discrete(name="") +
-  theme_bw(9) # remove background gray grid
+  
+  geom_point(aes(color = core_status)) + # in theory I would have it coloured by significance and just label the core but there is no core
+  ggrepel::geom_label_repel(data = subset(HIV_vol, padj < 0.05 & abs(log2FoldChange) > 2 & core_status != "Non-core"), 
+                            aes(label = Genus), 
+                            color = "black", 
+                            show.legend = FALSE, 
+                            max.overlaps = Inf) +
+  theme_bw()
+
 ggsave(filename="all_status_vol_plot.png",HIV_vol_plot)
 
 ## Bar graph
@@ -129,5 +184,6 @@ ggsave(filename="all_bar.png", all_bar)
 bars <- all_bar + PLWH_bar
 volcanos <- HIV_vol_plot + HIV_plwh_genus_vol_plot
 
-ggsave(filename="bars.png", bars, width = 3000, height = 2500, units = "px")
-ggsave(filename="volcanos.png", volcanos, width = 3000, height = 2500, units = "px")
+ggsave(filename="bars.png", bars)
+ggsave(filename="volcanos.png", volcanos)
+
